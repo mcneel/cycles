85cf0b8a54169ccab04ad88fe4f18cf5c50ca7cb
Nathan Letwory <nathan@mcneel.com>
2016-11-30 10:44:41 +0200

    Add Rhino-style texture mappings.

--- src/kernel/kernel_types.h
+++ src/kernel/kernel_types.h
@@ -1072,6 +1072,8 @@ typedef struct KernelCamera {
 	Transform worldtondc;
 	Transform worldtocamera;
 
+	Transform cameratondc;
+
 	MotionTransform motion;
 
 	/* Denotes changes in the projective matrix, namely in rastertocamera.
--- src/render/camera.h
+++ src/render/camera.h
@@ -154,6 +154,8 @@ public:
 	Transform worldtondc;
 	Transform worldtocamera;
 
+	Transform cameratondc;
+
 	Transform rastertocamera;
 	Transform cameratoraster;
 
