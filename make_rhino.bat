bdf8748e9544c5557fb41802983bc144ff00b759
Nathan Letwory <nathan@mcneel.com>
2023-04-21 10:50:53 +0300

    Add make_rhino.bat to turn of Hydra render delegate in debug build.

---
+++ make_rhino.bat
@@ -0,0 +1,29 @@
+@echo off
+
+REM Convenience wrapper for CMake commands
+
+setlocal enableextensions enabledelayedexpansion
+
+set BUILD_DIR=build
+set PYTHON=python
+set COMMAND=%1
+
+if "%COMMAND%" == "" (
+  set COMMAND=release
+)
+
+if "%COMMAND%" == "release" (
+	cmake -B %BUILD_DIR% -DWITH_CYCLES_ALEMBIC=OFF -DWITH_CYCLES_USD=OFF -DWITH_CYCLES_HYDRA_RENDER_DELEGATE=OFF && cd %BUILD_DIR% && cmake --build . --target install --config Release
+) else if "%COMMAND%" == "debug" (
+	cmake -B %BUILD_DIR% -DWITH_CYCLES_HYDRA_RENDER_DELEGATE=OFF -DWITH_CYCLES_USD=OFF && cd %BUILD_DIR% && cmake --build . --target install --config Debug
+) else if "%COMMAND%" == "clean" (
+	cd %BUILD_DIR% && cmake --build . --target install --config Clean
+) else if "%COMMAND%" == "test" (
+	cd %BUILD_DIR% && ctest --config Release
+) else if "%COMMAND%" == "update" (
+	%PYTHON% src/cmake/make_update.py
+) else if "%COMMAND%" == "format" (
+	%PYTHON% src/cmake/make_format.py
+) else (
+  echo Command "%COMMAND%" unknown
+)
