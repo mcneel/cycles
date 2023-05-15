150cf07513a432ccf6b9f63708b6a8d5c7b212f1
David Eränen <david.eranen@mcneel.com>
2023-05-15 13:13:26 +0300

    Update CyclesX build script to support Release builds

--- build_cycles_for_rhino.ps1
+++ build_cycles_for_rhino.ps1
@@ -1,3 +1,9 @@
+param (
+    [Parameter(Mandatory=$true)]
+    [ValidateSet('Debug','Release')]
+    [string]$buildConfig
+)
+
 $cwd = $PSScriptRoot
 
 Import-Module 'C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\Tools\Microsoft.VisualStudio.DevShell.dll'
@@ -5,22 +11,22 @@ Import-Module 'C:\Program Files\Microsoft Visual Studio\2022\Professional\Common
 Enter-VsDevShell -VsInstallPath "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools" -StartInPath $cwd
 
 $cycles_lib_dir = (Convert-Path "$cwd\..\lib")
-$install_location_debug = (Convert-Path "$cwd\..\..\..\..\..\bin\Debug\Plug-ins")
+$install_location = (Convert-Path "$cwd\..\..\..\..\..\bin\$buildConfig\Plug-ins")
 
 $dependencies = (Get-ChildItem -Recurse -Path $cycles_lib_dir -Include "OpenColorIO*dll", "openvdb.dll", "Imath*.dll", "boost_thread*dll", "OpenImageIO*dll", "tbb*dll", "OpenEXR*dll", "Iex*dll", "Ilm*dll")
 
 Push-Location $cwd
 
-.\make_rhino.bat debug
+.\make_rhino.bat $buildConfig.ToLower()
 
 Pop-Location
 
 foreach($dependency in $dependencies)
 {
     Write-Host "Copying $dependency"
-	Copy-Item "$dependency" "$install_location_debug\."
+    Copy-Item "$dependency" "$install_location\."
 }
 
-$ccycles_files = Get-ChildItem "$cwd\build\bin\Debug\ccycl*"
+$ccycles_files = Get-ChildItem "$cwd\build\bin\$buildConfig\ccycl*"
 Write-Host "Copying $ccycles_files"
-Copy-Item "$cwd\build\bin\Debug\ccycl*" "$install_location_debug\."
+Copy-Item "$cwd\build\bin\$buildConfig\ccycl*" "$install_location\."
