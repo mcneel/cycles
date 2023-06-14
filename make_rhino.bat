3a5b667bca66e3a144504fc40f6fb5d076160346
David Eränen <david.eranen@mcneel.com>
2023-06-14 15:27:04 +0300

    Fix OneAPI compile issues - Fix MSVC_REDIST_DIR to use forward slashes so that string-replace command succeeds. - Set WINDOWS_KITS_DIR. - Add message at end of make_rhino.bat script to guide user on what to do if they get build error where __cplusplus macro is set to the wrong value.

--- make_rhino.bat
+++ make_rhino.bat
@@ -13,7 +13,8 @@ set OPTIX="C:\ProgramData\NVIDIA Corporation\OptiX SDK 7.3.0"
 set LEVELZERO_INC="..\lib\win64_vc15\level-zero\include"
 set LEVELZERO_LIB="..\lib\win64_vc15\level-zero\lib"
 set OCLOC="C:\ProgramData\Intel\ocloc_win_dgpu_101.3802"
-set MSVC_REDIST_DIR="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Redist\MSVC\v142"
+set MSVC_REDIST_DIR="C:/Program Files (x86)/Microsoft Visual Studio/2019/Professional/VC/Redist/MSVC/14.29.30133"
+set WINDOWS_KITS_DIR="C:/Program Files (x86)/Windows Kits/10"
 
 set BUILD_CUDA_CMD= ^
 	-DWITH_CYCLES_CUDA_BINARIES=ON ^
@@ -27,16 +28,17 @@ if not "%ARG1%" == "cuda" (
 	)
 )
 
+set BUILDING_ONEAPI="Yes"
 set BUILD_ONEAPI_CMD= ^
 	-DWITH_CYCLES_DEVICE_ONEAPI=ON ^
-	-DWITH_CYCLES_ONEAPI_BINARIES=ON ^
 	-D_LEVEL_ZERO_INCLUDE_DIR=%LEVELZERO_INC% ^
 	-D_LEVEL_ZERO_LIBRARY=%LEVELZERO_LIB% ^
-	-DOCLOC_INSTALL_DIR=%OCLOC% ^
-	-DMSVC_REDIST_DIR=%MSVC_REDIST_DIR%
+	-DMSVC_REDIST_DIR=%MSVC_REDIST_DIR% ^
+	-DWINDOWS_KITS_DIR=%WINDOWS_KITS_DIR%
 
 if not "%ARG1%" == "oneapi" (
 	if not "%ARG1%" == "all" (
+		set BUILDING_ONEAPI="No"
 		set BUILD_ONEAPI_CMD=""
 	)
 )
@@ -59,6 +61,8 @@ if "%COMMAND%" == "release" (
 	-DWITH_CYCLES_ALEMBIC=OFF ^
 	-DWITH_CYCLES_USD=OFF ^
 	-DWITH_CYCLES_HYDRA_RENDER_DELEGATE=OFF ^
+	-DWITH_CYCLES_CUDA_BINARIES=OFF ^
+	-DWITH_CYCLES_DEVICE_OPTIX=OFF ^
 	%BUILD_CUDA_CMD% ^
 	%BUILD_ONEAPI_CMD% ^
 	%BUILD_HIP_CMD% ^
@@ -86,4 +90,12 @@ if "%COMMAND%" == "release" (
 	%PYTHON% src/cmake/make_format.py
 ) else (
   echo Command "%COMMAND%" unknown
+)
+
+if %BUILDING_ONEAPI% == "Yes" ( 
+  echo **************************************************************************************************************
+  echo NOTE: If you got compile errors like "error C2338: DPCPP does not support C++ version earlier than C++17.",  *
+  echo then do the following: Add the /Zc:__cplusplus compiler option to the cycles_device.vcxproj compiler options *
+  echo to force the compiler to set the correct value to the macro.                                                 *
+  echo **************************************************************************************************************
 )
