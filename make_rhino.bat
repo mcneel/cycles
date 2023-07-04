e18f624cdbcd031ac1f44bda54a3b827418ca78e
David Eränen <david.eranen@mcneel.com>
2023-07-04 10:45:09 +0300

    Improve build scripts and documentation - Add steps on how to update ccycles.dll/.dylib to README.md. - Added better defaults to make_rhino.bat script. - Build with CUDA .PTX by default from Rhino.sln PowerShell script.

--- make_rhino.bat
+++ make_rhino.bat
@@ -15,15 +15,36 @@ set LEVELZERO_LIB="..\lib\win64_vc15\level-zero\lib"
 set MSVC_REDIST_DIR="C:/Program Files (x86)/Microsoft Visual Studio/2019/Professional/VC/Redist/MSVC/14.29.30133"
 set WINDOWS_KITS_DIR="C:/Program Files (x86)/Windows Kits/10"
 
+REM Supported build configs:
+REM    debug          # Build with CPU-only support
+REM    debug cuda     # Build .PTX files 
+REM    debug cudabin  # Build all CUDA binaries
+REM    debug oneapi   # Build with OneAPI support
+REM    debug hip      # Build with HIP support
+REM    debug all      # Build with support for all devices (+ .PTX files)
+REM    debug allbin   # Build with support for all devices (+ all CUDA binaries)
+
 set BUILD_CUDA_CMD= ^
 	-DWITH_CYCLES_CUDA_BINARIES=ON ^
 	-DWITH_CYCLES_DEVICE_OPTIX=ON ^
-	-DCYCLES_CUDA_BINARIES_ARCH="sm_37;sm_50;sm_52;sm_60;sm_61;sm_70;sm_75;sm_86;compute_75" ^
+	-DCYCLES_CUDA_BINARIES_ARCH="compute_52" ^
 	-DOPTIX_ROOT_DIR=%OPTIX%
 
 if not "%ARG1%" == "cuda" (
 	if not "%ARG1%" == "all" (
 	  set BUILD_CUDA_CMD=""
+	)
+)
+
+set BUILD_CUDABIN_CMD= ^
+	-DWITH_CYCLES_CUDA_BINARIES=ON ^
+	-DWITH_CYCLES_DEVICE_OPTIX=ON ^
+	-DCYCLES_CUDA_BINARIES_ARCH="sm_37;sm_50;sm_52;sm_60;sm_61;sm_70;sm_75;sm_86;compute_75" ^
+	-DOPTIX_ROOT_DIR=%OPTIX%
+	
+if not "%ARG1%" == "cudabin" (
+	if not "%ARG1%" == "allbin" (
+		set BUILD_CUDABIN_CMD=""
 	)
 )
 
@@ -52,22 +73,18 @@ if not "%ARG1%" == "hip" (
 )
 
 if "%COMMAND%" == "" (
-  set COMMAND=release
+	set COMMAND=release
 )
 
 if "%COMMAND%" == "release" (
-	cmake -B %BUILD_DIR% ^
-	-DWITH_CYCLES_ALEMBIC=OFF ^
-	-DWITH_CYCLES_USD=OFF ^
-	-DWITH_CYCLES_HYDRA_RENDER_DELEGATE=OFF ^
-	-DWITH_CYCLES_CUDA_BINARIES=OFF ^
-	-DWITH_CYCLES_DEVICE_OPTIX=OFF ^
-	%BUILD_CUDA_CMD% ^
-	%BUILD_ONEAPI_CMD% ^
-	%BUILD_HIP_CMD% ^
-	&& cd %BUILD_DIR% && cmake --build . --target install --config Release
-
+	set BUILDING=Yes
+	set CONFIG=Release
 ) else if "%COMMAND%" == "debug" (
+	set BUILDING=Yes
+	set CONFIG=Debug
+)
+
+if "%BUILDING%" == "Yes" (
 	cmake -B %BUILD_DIR% ^
 	-DWITH_CYCLES_ALEMBIC=OFF ^
 	-DWITH_CYCLES_USD=OFF ^
@@ -75,10 +92,10 @@ if "%COMMAND%" == "release" (
 	-DWITH_CYCLES_CUDA_BINARIES=OFF ^
 	-DWITH_CYCLES_DEVICE_OPTIX=OFF ^
 	%BUILD_CUDA_CMD% ^
+	%BUILD_CUDABIN_CMD% ^
 	%BUILD_ONEAPI_CMD% ^
 	%BUILD_HIP_CMD% ^
-	&& cd %BUILD_DIR% && cmake --build . --target install --config Debug
-
+	&& cd %BUILD_DIR% && cmake --build . --target install --config %CONFIG%
 ) else if "%COMMAND%" == "clean" (
 	cd %BUILD_DIR% && cmake --build . --target install --config Clean
 ) else if "%COMMAND%" == "test" (
