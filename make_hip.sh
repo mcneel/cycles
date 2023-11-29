9266d483cd4bacb754b6f786dcf09a6b0e5eeadd
Nathan Letwory <nathan@letworyinteractive.com>
2023-11-29 07:11:11 +0200

    Adjust HIP Docker image and build script.
    Support RDNA3
    Update to ROCm 5.7.1

--- make_hip.sh
+++ make_hip.sh
@@ -6,6 +6,8 @@ BUILD_DIR=build_hip
 PYTHON=python3
 COMMAND=$1
 
+#rm -rf $BUILD_DIR
+
 if [ -z "$COMMAND" ]
 then
   COMMAND=Release
@@ -29,6 +31,6 @@ cmake -B $BUILD_DIR \
 -DWITH_CUDA_DYNLOAD=OFF \
 -DWITH_CYCLES_DEVICE_HIP=ON \
 -DWITH_CYCLES_HIP_BINARIES=ON \
--DCYCLES_HIP_BINARIES_ARCH="gfx900;gfx906;gfx90c;gfx902;gfx1010;gfx1011;gfx1012;gfx1030;gfx1031;gfx1032;gfx1034;gfx1035;gfx1036" \
--DHIP_HIPCC_EXECUTABLE="/opt/rocm-5.5.1/bin/hipcc" \
+-DCYCLES_HIP_BINARIES_ARCH="gfx900;gfx906;gfx90c;gfx902;gfx1010;gfx1011;gfx1012;gfx1030;gfx1031;gfx1032;gfx1034;gfx1035;gfx1036;gfx1100;gfx1101;gfx1102;gfx1103" \
+-DHIP_HIPCC_EXECUTABLE="/opt/rocm-5.7.1/bin/hipcc" \
 && cd $BUILD_DIR && cmake --build . -j 8 --target install --config $COMMAND
