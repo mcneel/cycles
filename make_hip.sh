d8c0aee6f422bb81f94bb88d448ed9e53b655333
Nathan Letwory <nathan@letworyinteractive.com>
2024-10-04 21:28:36 +0300

    HIP compute model 11.5 support.
    * Update Docker image to RocM 6.2.2
    * Add HIP architectures supported by RocM 6.2.2
    * Ensure image stays on Ubuntu 22.04 until we
      explicitly roll to newer Linux version.

--- make_hip.sh
+++ make_hip.sh
@@ -31,6 +31,6 @@ cmake -B $BUILD_DIR \
 -DWITH_CUDA_DYNLOAD=OFF \
 -DWITH_CYCLES_DEVICE_HIP=ON \
 -DWITH_CYCLES_HIP_BINARIES=ON \
--DCYCLES_HIP_BINARIES_ARCH="gfx900;gfx906;gfx90c;gfx902;gfx1010;gfx1011;gfx1012;gfx1030;gfx1031;gfx1032;gfx1034;gfx1035;gfx1036;gfx1100;gfx1101;gfx1102;gfx1103" \
--DHIP_HIPCC_EXECUTABLE="/opt/rocm-5.7.1/bin/hipcc" \
+-DCYCLES_HIP_BINARIES_ARCH="gfx900;gfx906;gfx90c;gfx902;gfx1010;gfx1011;gfx1012;gfx1030;gfx1031;gfx1032;gfx1034;gfx1035;gfx1036;gfx1100;gfx1101;gfx1102;gfx1103;gfx1150;gfx1151;gfx1152;gfx1200;gfx1201" \
+-DHIP_HIPCC_EXECUTABLE="/opt/rocm-6.2.2/bin/hipcc" \
 && cd $BUILD_DIR && cmake --build . -j 8 --target install --config $COMMAND
